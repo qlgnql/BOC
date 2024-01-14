c85d5de7ba52a27dffca9db1210efb1b1bbf6784
youngile <ddffgghh22@naver.com>
2024-01-14 15:19:22 +0900

    Modified Dashboard, MusicPlayer js and css

--- src/MusicPlayer.js
+++ src/MusicPlayer.js
@@ -1,27 +1,23 @@
-// MusicPlayer.js
-
 import React, { useState, useRef, useEffect } from 'react';
 import './MusicPlayer.css';
 
 const MusicPlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [volume, setVolume] = useState(1);
-  const [selectedSong, setSelectedSong] = useState(null);
+  const [currentSongIndex, setCurrentSongIndex] = useState(null);
   const [hasStarted, setHasStarted] = useState(false);
-  const [likedSongs, setLikedSongs] = useState([]);
   const [currentTime, setCurrentTime] = useState(0);
   const audioRef = useRef(null);
   const albumCoverRef = useRef(null);
 
   const songList = [
-    { title: '첫눈', file: process.env.PUBLIC_URL + '/first-snow.mp3', cover: '/image/first-snow.jpg' },
-    { title: 'Stay', file: process.env.PUBLIC_URL + '/stay.mp3', cover: '/image/stay.jpg' },
-    { title: '입술', file: process.env.PUBLIC_URL + '/lips.mp3', cover: '/image/lips.jpg' },
-    { title: '다정히내이름을부르면', file: process.env.PUBLIC_URL + '/callmyname.mp3', cover: '/image/callmyname.jpg' },
-    { title: '인사', file: process.env.PUBLIC_URL + '/hello.mp3', cover: '/image/hello.jpg' },
-    { title: 'Super shy', file: process.env.PUBLIC_URL + '/supershy.mp3', cover: '/image/supershy.jpg' },
-    { title: '파이팅 해야지', file: process.env.PUBLIC_URL + '/fighting.mp3', cover: '/image/fighting.jpg' },
-    
+    { title: 'The Kid LAROI, Justin Bieber - Stay', file: process.env.PUBLIC_URL + '/stay.mp3', cover: '/image/stay.jpg' },
+    { title: 'EXO - 첫눈', file: process.env.PUBLIC_URL + '/first-snow.mp3', cover: '/image/first-snow.jpg' },
+    { title: '헤이즈(Feat.10CM) - 입술', file: process.env.PUBLIC_URL + '/lips.mp3', cover: '/image/lips.jpg' },
+    { title: '경서예지 X 전건호 - 다정히 내 이름을 부르면', file: process.env.PUBLIC_URL + '/callmyname.mp3', cover: '/image/callmyname.jpg' },
+    { title: '범진 - 인사', file: process.env.PUBLIC_URL + '/hello.mp3', cover: '/image/hello.jpg' },
+    { title: 'NEWJEANS - Super shy', file: process.env.PUBLIC_URL + '/supershy.mp3', cover: '/image/supershy.jpg' },
+    { title: '부석순 - 파이팅 해야지', file: process.env.PUBLIC_URL + '/fighting.mp3', cover: '/image/fighting.jpg' },
     // ... (이전 코드는 그대로 유지)
   ];
 
@@ -51,29 +47,15 @@ const MusicPlayer = () => {
 
   const handleSongChange = (e) => {
     const selectedIndex = e.target.value;
-    const newSelectedSong = songList[selectedIndex];
+    const newSelectedIndex = parseInt(selectedIndex, 10);
 
     if (isPlaying) {
       audioRef.current.pause();
       setIsPlaying(false);
     }
 
-    setSelectedSong(newSelectedSong);
-    audioRef.current.src = newSelectedSong.file;
-    audioRef.current.load();
-
+    setCurrentSongIndex(newSelectedIndex);
     setHasStarted(true);
-  };
-
-  const toggleLike = () => {
-    if (selectedSong) {
-      const isLiked = likedSongs.includes(selectedSong.title);
-      if (isLiked) {
-        setLikedSongs(likedSongs.filter(songTitle => songTitle !== selectedSong.title));
-      } else {
-        setLikedSongs([...likedSongs, selectedSong.title]);
-      }
-    }
   };
 
   const handleTimeUpdate = () => {
@@ -84,29 +66,37 @@ const MusicPlayer = () => {
 
     const progress = (audio.currentTime / audio.duration) * 100;
     progressBar.style.width = progress + '%';
+  };
 
-    if (audio.currentTime === audio.duration) {
-      // 현재 재생 중인 곡이 끝났을 때 다음 곡으로 전환
-      const currentIndex = songList.findIndex(song => song === selectedSong);
-      const nextIndex = (currentIndex + 1) % songList.length;
-      const nextSong = songList[nextIndex];
+  const handleSongEnded = () => {
+    playNextSong();
+  };
 
-      setSelectedSong(nextSong);
-      audioRef.current.src = nextSong.file;
-      audioRef.current.load();
-      audioRef.current.play().catch(error => console.error('Audio playback error:', error));
-      setIsPlaying(true);
-    }
+  const playNextSong = () => {
+    const nextIndex = (currentSongIndex + 1) % songList.length;
+
+    setCurrentSongIndex(nextIndex);
+
+    const selectElement = document.getElementById('song-select');
+    selectElement.value = nextIndex.toString();
+
+    audioRef.current.src = songList[nextIndex].file;
+    audioRef.current.load();
+    audioRef.current.play().catch(error => console.error('Audio playback error:', error));
+    setIsPlaying(true);
   };
 
   useEffect(() => {
     const audio = audioRef.current;
+
     audio.addEventListener('timeupdate', handleTimeUpdate);
+    audio.addEventListener('ended', handleSongEnded);
 
     return () => {
       audio.removeEventListener('timeupdate', handleTimeUpdate);
+      audio.removeEventListener('ended', handleSongEnded);
     };
-  }, []);
+  }, [currentSongIndex, hasStarted]);
 
   useEffect(() => {
     const albumCover = albumCoverRef.current;
@@ -121,30 +111,27 @@ const MusicPlayer = () => {
   useEffect(() => {
     const audio = audioRef.current;
 
-    if (selectedSong && !isPlaying) {
+    if (currentSongIndex !== null && !isPlaying) {
       audio.play().catch(error => console.error('Audio playback error:', error));
       setIsPlaying(true);
     }
-  }, [selectedSong]);
+  }, [currentSongIndex]);
 
   return (
-    <div style={{ position: 'fixed', bottom: 0, left: 0, width: '100%', height: '235px', backgroundColor: '#F8F8FF', padding: '10px', color: '#000000' }}>
-      <h3 style={{ marginBottom: '-10px' }}>
+    <div style={{ position: 'fixed', bottom: 0, left: 0, width: '100%', height: '230px', backgroundColor: '#F8F8FF', padding: '10px', color: '#000000' }}>
+      <h3 style={{ marginBottom: '10px' }}>
         Music Player
-        <button onClick={toggleLike} style={{ marginLeft: '10px', cursor: 'pointer', fontSize: '16px', color: likedSongs.includes(selectedSong?.title) ? 'red' : 'black' }}>
-          &#10084; {/* Unicode heart character */}
-        </button>
       </h3>
       <div ref={albumCoverRef} className={`album-cover`}>
-        {selectedSong && <img src={selectedSong.cover} alt="Album Cover" />}
+        {songList[currentSongIndex] && <img src={songList[currentSongIndex].cover} alt="Album Cover" />}
       </div>
-      <select onChange={handleSongChange} defaultValue="default">
+      <select id="song-select" onChange={handleSongChange} defaultValue="default">
         <option value="default" disabled>Select a song</option>
         {songList.map((song, index) => (
           <option key={index} value={index}>{song.title}</option>
         ))}
       </select>
-      <audio ref={audioRef} src={selectedSong ? selectedSong.file : ''} />
+      <audio ref={audioRef} src={songList[currentSongIndex] ? songList[currentSongIndex].file : ''} />
       <button onClick={togglePlay}>{isPlaying ? 'Pause' : 'Play'}</button>
       <input
         type="range"
@@ -155,11 +142,9 @@ const MusicPlayer = () => {
         onChange={handleVolumeChange}
       />
       <div>
-        {/* Progress bar */}
         <div id="progress-bar-container" style={{ width: '100%', height: '2px', backgroundColor: '#ddd', marginTop: '-2px', position: 'relative' }}>
           <div id="progress-bar" style={{ height: '100%', backgroundColor: '#000000', width: '0%' }}></div>
         </div>
-        {/* Current time / Total time */}
         {currentTime > 0 && !isNaN(audioRef.current.duration) && (
           <span>{formatTime(currentTime)} / {formatTime(audioRef.current.duration)}</span>
         )}
